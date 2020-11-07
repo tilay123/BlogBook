3dd8f9bebdbd5237e6a9e67abab7c4af818d68ec
Til Baniya <tilbaniya@Tils-MacBook-Pro-2.local>
2020-11-07 01:44:15 -0500

    made website more interactive... I am still working on uploading images part

--- dist/homepageHelper.dev.js
+++ dist/homepageHelper.dev.js
@@ -31,4 +31,74 @@ db.collection("users").get().then(function (querySnapshot) {
   $("#listing").html(htmlCode);
 })["catch"](function (error) {
   console.log("Error getting documents: ", error);
+}); // When user submit a post below function will execute. If both image and description exists then it will upload
+// it to the firestore and firebase storage
+
+$("#submit_post_button").click(function _callee2() {
+  var file, description, userId, _db, time, storageRef, fileName, uploadTask;
+
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          file = $("#postPic").prop("files")[0];
+          description = $("#postText").val();
+
+          if (file == null || !description) {
+            alert("Image or description cannot be empty.");
+          } else {
+            // If both image and description exist.
+            userId = firebase.auth().currentUser.uid;
+            _db = firebase.firestore();
+            time = new Date();
+            storageRef = firebase.storage().ref(); // firestore reference
+
+            fileName = time.getTime() + "_" + file.name;
+            uploadTask = storageRef.child("blogImages/" + fileName).put(file); // uploading picture to firestorage
+            // Documentation: https://firebase.google.com/docs/storage/web/upload-files
+
+            uploadTask.on("state_changed", function (snapshot) {//about upload status
+            }, function (error) {
+              // if error
+              alert("Theres an error uploading your image. Error Message:" + error.message);
+            }, function () {
+              // if successful
+              uploadTask.snapshot.ref.getDownloadURL().then(function _callee(downloadUrl) {
+                var blogData;
+                return regeneratorRuntime.async(function _callee$(_context) {
+                  while (1) {
+                    switch (_context.prev = _context.next) {
+                      case 0:
+                        // after we have the download url of the picture
+                        blogData = {
+                          "imageUrl": downloadUrl,
+                          "description": description,
+                          "uid": userId,
+                          "dateUploded": time.toLocaleString()
+                        };
+                        _context.next = 3;
+                        return regeneratorRuntime.awrap(_db.collection('blogs').doc(userId).collection('userBlogs').doc().set(blogData)["catch"](function (error) {
+                          alert("Error uploading user Data:" + error.message);
+                        }));
+
+                      case 3:
+                        // after uploading the post reset the form.
+                        $("#blogForm")[0].reset();
+
+                      case 4:
+                      case "end":
+                        return _context.stop();
+                    }
+                  }
+                });
+              });
+            });
+          }
+
+        case 3:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
 });
--- homepageHelper.js
+++ homepageHelper.js
@@ -1,32 +1,32 @@
 // // Initializing Firebase project
 var firebaseConfig = {
-    apiKey: "AIzaSyABOwyUGeAWpwaPro1iPmIJ3A-wGXaiBv0",
-    authDomain: "postbook-f5b68.firebaseapp.com",
-    databaseURL: "https://postbook-f5b68.firebaseio.com",
-    projectId: "postbook-f5b68",
-    storageBucket: "postbook-f5b68.appspot.com",
-    messagingSenderId: "1075694270728",
-    appId: "1:1075694270728:web:89e70eee474b1ffa16d4bd",
-    measurementId: "G-LN06ME0J6J"
-  };
-  // Initialize Firebase
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-  }
+  apiKey: "AIzaSyABOwyUGeAWpwaPro1iPmIJ3A-wGXaiBv0",
+  authDomain: "postbook-f5b68.firebaseapp.com",
+  databaseURL: "https://postbook-f5b68.firebaseio.com",
+  projectId: "postbook-f5b68",
+  storageBucket: "postbook-f5b68.appspot.com",
+  messagingSenderId: "1075694270728",
+  appId: "1:1075694270728:web:89e70eee474b1ffa16d4bd",
+  measurementId: "G-LN06ME0J6J"
+};
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
 
-  // TIL: This is what makes a website dynamic. In the code below I am fetching live data from firebase firestore.
-  // I am retrieving all the users data and then displaying them
-  const db = firebase.firestore()
+// TIL: This is what makes a website dynamic. In the code below I am fetching live data from firebase firestore.
+// I am retrieving all the users data and then displaying them
+const db = firebase.firestore()
 // documentation: https://firebase.google.com/docs/firestore/query-data/get-data#get_multiple_documents_from_a_collection
 var htmlCode = ""
 db.collection("users")
   .get()
-  .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-          //console.log(doc.id, " => ", doc.data());
-            htmlCode +=  // making Rudolph's html code dynamically generated
-            `
+  .then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      //console.log(doc.id, " => ", doc.data());
+      htmlCode +=  // making Rudolph's html code dynamically generated
+        `
             <div>
                 <li class="outerli"><ul class="innerul"> 
                 <li><h3>${doc.data().firstName + " " + doc.data().lastName}</h3></li>
@@ -34,9 +34,67 @@ db.collection("users")
                 <li><b>Bio: </b>${doc.data().bio}</li></ul></li>
             </div>
             `
-      });
-      $("#listing").html(htmlCode)
+    });
+    $("#listing").html(htmlCode)
   })
-  .catch(function(error) {
-      console.log("Error getting documents: ", error);
+  .catch(function (error) {
+    console.log("Error getting documents: ", error);
   });
+
+// When user submit a post below function will execute. If both image and description exists then it will upload
+// it to the firestore and firebase storage
+$("#submit_post_button").click(async () => {
+
+  const file = $("#postPic").prop("files")[0]
+  const description = $("#postText").val()
+
+  if (file == null || !description) {
+    alert("Image or description cannot be empty.")
+  } else { // If both image and description exist.
+    const userId = firebase.auth().currentUser.uid
+    const db = firebase.firestore();
+
+    const time = new Date()
+    const storageRef = firebase.storage().ref() // firestore reference
+    const fileName = time.getTime() + "_" + file.name
+
+    const uploadTask = storageRef.child("blogImages/" + fileName).put(file)
+
+    // uploading picture to firestorage
+    // Documentation: https://firebase.google.com/docs/storage/web/upload-files
+    uploadTask.on("state_changed",
+      function (snapshot) {
+        //about upload status
+      },
+      function (error) {
+        // if error
+        alert("Theres an error uploading your image. Error Message:" + error.message)
+      }, function () {
+        // if successful
+
+        uploadTask.snapshot.ref.getDownloadURL().then(async function (downloadUrl) {
+          // after we have the download url of the picture
+          const blogData = {
+            "imageUrl": downloadUrl,
+            "description": description,
+            "uid": userId,
+            "dateUploded": time.toLocaleString()
+          }
+
+          await db.collection('blogs').doc(userId).collection('userBlogs').doc().set(blogData).catch(function (error) {
+            alert("Error uploading user Data:" + error.message)
+          });
+          // after uploading the post reset the form.
+          $("#blogForm")[0].reset()
+        })
+      }
+    )
+
+
+
+
+
+  }
+
+
+})
